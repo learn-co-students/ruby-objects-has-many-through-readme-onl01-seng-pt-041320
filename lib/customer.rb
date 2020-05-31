bba95bf806b49aef186295cfaa0f3f6992fdc956
jonah <benjaminjmallard@gmail.com>
2020-05-31 16:02:44 -0500

    Done.

--- lib/customer.rb
+++ lib/customer.rb
@@ -27,17 +27,7 @@ end
   meals.map do |meal|
     meal.waiter
   end
+
 end
-
-  def meals
-  Meal.all.select do |meal|
-    meal.waiter == self #checking for waiter now
-  end
-end
-
-
-
-
-
 
 end
